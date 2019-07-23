4c6f3d155cfcfeccf9d5bf63e7bb8b005bf1154a
TimHaidel <tsimur.haidel@gmail.com>
2019-07-23 17:37:23 +0300

    no message

--- web/src/main/java/by/itacademy/jd2/th/messenger/web/controller/DefaultController.java
+++ web/src/main/java/by/itacademy/jd2/th/messenger/web/controller/DefaultController.java
@@ -1,16 +1,63 @@
 package by.itacademy.jd2.th.messenger.web.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import by.itacademy.jd2.th.messenger.dao.api.entity.table.IUserAccount;
+import by.itacademy.jd2.th.messenger.dao.api.filter.UserAccountFilter;
+import by.itacademy.jd2.th.messenger.service.IUserAccountService;
+import by.itacademy.jd2.th.messenger.web.converter.UserAccountToDTOConverter;
+import by.itacademy.jd2.th.messenger.web.dto.UserAccountDTO;
+import by.itacademy.jd2.th.messenger.web.dto.grid.GridStateDTO;
 
 @Controller
 @RequestMapping(value = "/")
-public class DefaultController {
+public class DefaultController extends AbstractController {
+
+	private IUserAccountService userAccountService;
+	private UserAccountToDTOConverter toDtoConverter;
+
+	public DefaultController(IUserAccountService userAccountService, UserAccountToDTOConverter toDtoConverter) {
+		super();
+		this.userAccountService = userAccountService;
+		this.toDtoConverter = toDtoConverter;
+	}
+
+//	@RequestMapping(method = RequestMethod.GET)
+//	public String index() {
+//		return "home";
+//	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String index() {
-		return "home";
+	public ModelAndView index(final HttpServletRequest req,
+			@RequestParam(name = "page", required = false) final Integer pageNumber,
+			@RequestParam(name = "sort", required = false) final String sortColumn) {
+
+		final GridStateDTO gridState = getListDTO(req);
+		gridState.setPage(pageNumber);
+		gridState.setSort(sortColumn, "id");
+
+		final UserAccountFilter filter = new UserAccountFilter();
+		prepareFilter(gridState, filter);
+
+		final List<IUserAccount> entities = userAccountService.find(filter);
+		List<UserAccountDTO> dtos = entities.stream().map(toDtoConverter).collect(Collectors.toList());
+		gridState.setTotalCount(userAccountService.getCount(filter));
+
+		final Map<String, Object> models = new HashMap<>();
+		models.put("gridItems", dtos);
+		
+		return new ModelAndView("home", models);
 	}
 
 }
--- web/src/main/java/by/itacademy/jd2/th/messenger/web/dto/ContactDTO.java
+++ web/src/main/java/by/itacademy/jd2/th/messenger/web/dto/ContactDTO.java
@@ -3,44 +3,60 @@ package by.itacademy.jd2.th.messenger.web.dto;
 import java.util.Date;
 
 public class ContactDTO {
-	
+
 	Integer id;
 	Integer initiatorId;
 	Integer acceptorId;
 	Integer status;
+	String avatar;
 	Date created;
-	
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getInitiatorId() {
 		return initiatorId;
 	}
+
 	public void setInitiatorId(Integer initiatorId) {
 		this.initiatorId = initiatorId;
 	}
+
 	public Integer getAcceptorId() {
 		return acceptorId;
 	}
+
 	public void setAcceptorId(Integer acceptorId) {
 		this.acceptorId = acceptorId;
 	}
+
 	public Integer getStatus() {
 		return status;
 	}
+
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
+
 	public Date getCreated() {
 		return created;
 	}
+
 	public void setCreated(Date created) {
 		this.created = created;
 	}
-	
-	
 
 }
