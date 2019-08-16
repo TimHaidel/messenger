dd57bb020a13ed988c75bda791e42bd61962c9ed
TimHaidel <tsimur.haidel@gmail.com>
2019-08-16 19:51:17 +0300

    no message

--- dao-jdbc-impl/src/main/java/by/itacademy/jd2/th/messenger/jdbc/impl/entity/UserGroup.java
+++ dao-jdbc-impl/src/main/java/by/itacademy/jd2/th/messenger/jdbc/impl/entity/UserGroup.java
@@ -3,32 +3,32 @@ package by.itacademy.jd2.th.messenger.jdbc.impl.entity;
 import by.itacademy.jd2.th.messenger.dao.api.entity.table.IUserGroup;
 
 public class UserGroup extends BaseEntity implements IUserGroup {
-    private String name;
-    private int status;
+	private String name;
+	private int status;
 
-    @Override
-    public String getName() {
-        return name;
-    }
+	@Override
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public void setName(final String name) {
-        this.name = name;
-    }
+	@Override
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    @Override
-    public int getStatus() {
-        return status;
-    }
+	@Override
+	public Integer getStatus() {
+		return status;
+	}
 
-    @Override
-    public void setStatus(final int status) {
-        this.status = status;
-    }
+	@Override
+	public void setStatus(final int status) {
+		this.status = status;
+	}
 
-    @Override
-    public String toString() {
-        return "UserGroup [name=" + name + ", status=" + status + "]";
-    }
+	@Override
+	public String toString() {
+		return "UserGroup [name=" + name + ", status=" + status + "]";
+	}
 
 }
--- web/src/test/java/StartJetty.java
+++ web/src/test/java/StartJetty.java
@@ -1,11 +1,17 @@
+import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.Configuration.ClassList;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
 
 /**
  * Separate startup class for people that want to run the examples directly. Use
@@ -53,6 +59,13 @@ public final class StartJetty {
 		bb.setWar("src/main/webapp");
 
 		server.setHandler(bb);
+
+		final EnvConfiguration envConfiguration = new EnvConfiguration();
+		final URL url = new File("src/test/resources/jetty-env.xml").toURI().toURL();
+		envConfiguration.setJettyEnvXml(url);
+		final Configuration[] configurations = new Configuration[] { new WebInfConfiguration(), envConfiguration,
+				new WebXmlConfiguration() };
+		bb.setConfigurations(configurations);
 
 		try {
 			server.start();
